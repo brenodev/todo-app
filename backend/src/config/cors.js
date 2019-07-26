0452ec4c9d937cb3d9206355893dd78782e257fc
Breno Guilherme de Souza Lopes <bg.souza.lopes@hotmail.com>
2019-07-26 18:14:34 -0300

    Habilitando o Cors da aplicação

---
+++ backend/src/config/cors.js
@@ -0,0 +1,8 @@
+// Middleware para requisição, response e next
+module.exports = function (req, res, next) {  
+// CABEÇALHOS DO CORS
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+}
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,12 +3,12 @@ const port = 3003 // porta de inicialização
 const bodyParser = require('body-parser') // parse do corpo da requisição 
 const express = require('express') // servidor web que roda em cima do node
 const server = express() // start express
-// const allowCors = require('./cors')
+const allowCors = require('./cors') // referencia arquivo do cors
 
 // Middlewares
 server.use(bodyParser.urlencoded({ extended: true })) // para requisições todas requisições que chegam no servidor
 server.use(bodyParser.json()) 
-// server.use(allowCors)
+server.use(allowCors) // permite que a requisição da aplicação possa vir de uma origem diferente
 
 // registrando a porta 3003
 server.listen(port, function() {
