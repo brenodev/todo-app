134f5fbafb95a5ec9ee2d6eac028b92fd0c719a2
Breno Guilherme de Souza Lopes <bg.souza.lopes@hotmail.com>
2019-07-26 15:35:36 -0300

    configurando o servidor do Express e modificando algumas coisas do ambiente

---
+++ backend/src/config/server.js
@@ -0,0 +1,18 @@
+const port = 3003 // porta de inicialização
+
+const bodyParser = require('body-parser') // parse do corpo da requisição 
+const express = require('express') // servidor web que roda em cima do node
+const server = express() // start express
+// const allowCors = require('./cors')
+
+// Middlewares
+server.use(bodyParser.urlencoded({ extended: true })) // para requisições todas requisições que chegam no servidor
+server.use(bodyParser.json()) 
+// server.use(allowCors)
+
+// registrando a porta 3003
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
+})
+
+// module.exports = server
---
+++ backend/src/loader.js
@@ -0,0 +1,1 @@
+require('./config/server') // configuração do server para inicialização
