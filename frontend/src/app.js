8c99c7adb9ecdcaa39d1bbd3f33fe02666c6bfc7
Breno Guilherme de Souza Lopes <bg.souza.lopes@hotmail.com>
2019-08-01 15:28:56 -0300

    criado componente MENU

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,12 +1,12 @@
-'use strict'
 import React from 'react';
-
 import './app.css'
 import Todo from './components/Todo/Todo';
 import About from './components/About/About'
+import Menu from './template/Menu/Menu';
 
 const App = props => (
   <div className='container'>
+    <Menu/>
     <Todo />
     <About />
   </div>
